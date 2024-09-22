e9557d598b410245948c88c6e9ff3085dc71a948
Jagaran Chakma <jagaranchakma201@gmail.com>
2024-09-22 07:30:59 -0700

    Add files via upload

---
+++ task1/Main_new.java
@@ -0,0 +1,15 @@
+/*
+  This is the main entry point for the application.
+  It creates an instance of the MyFrame class, which likely initializes and displays the application's GUI.
+ */
+
+package ProjectNo1;
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+
+        // Create a new instance of MyFrame to launch the GUI
+        new MyFrame();  
+}
